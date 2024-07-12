0424471cc7de5d104df855949bcd203a870da8ab
MarenceSpeed <44441111mp@gmail.com>
2024-07-12 16:58:59 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -30,15 +30,25 @@ function displayInputs() {
     console.log("Displayed inputs:", answers);
 }
 
-document.getElementById('dugmemisao').addEventListener('click', function() {
-    // Create a new paragraph element
-    const newParagraph = document.createElement('p');
-    
-    // Add some text content to the new paragraph
-    newParagraph.textContent = 'This is a new paragraph.';
-    
-    // Append the new paragraph to the paragraph container
-    document.getElementById('paragraphContainer').appendChild(newParagraph);
-});
-
+const texts = [
+    "",
+    "Da...život je definitivno besmislen...nepravedan i nefer... I to je jasno svakome ko je iole zagrebao ispod površnog i banalnog... Memento mori...pa to da ne možemo zaštiti one koje volimo od patnje...pa tek zla sudbina za dobre ljude...i ostale teške priče koje su jednostavno kalibrisane datosti ovog univerzuma... Tajna je u nečemu drugom... Iako je život sam po sebi besmislen Naš Posao je da mu damo smisao... I tu nema kraja ljudskoj kreativnosti... Od monaha na Svetoj Gori... Preko ljudi zaljubljenih u materijalno... Umetnika i naučnika rešenih da promene svet... Do šankera na Ibici... I svega između... I svako treba da pronađe mesto baš za sebe na toj Lestvici... Baš tamo gde će njemu lično biti dobro...",
+    "Potpuno me je oduševila misao koju sam čuo pre neki dan na auto otpadu...od čoveka koji je sa mnom stajao u redu...nalakćen na svog razdrndanog mezimca...i čekao da nas obojicu odere i obrlati vlasnik auto otpada...polučovek...polueuro... Druže...sve je to lepo...avioni...kamioni...skupe limuzine...ali vrednost automobila se jedino meri po tome na koliko si mora i vikend putovanja sa njim otišao...sve ostalo je potpuno nebitno. Kakva duboka životna filozofija... Kakav nasmešeni i pozitivni Mudrac...sa najneočekivanijeg mogućeg mesta...",
+    "Ovo je treći tekst.",
+    "Potreba oba pertnera",
+    "Losa energija i razumevanje partnera",
+    "Ovo je četvrti tekst.",
+    "Ovo je Peti tekst.",
+  ];
+  
+  let currentIndex = 0;
+  
+  const textDiv = document.getElementById("textDiv");
+  const nextButton = document.getElementById("nextButton");
+  
+  nextButton.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1) % texts.length;
+    textDiv.innerText = texts[currentIndex];
+  });
+  
 
