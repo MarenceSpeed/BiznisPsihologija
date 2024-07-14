2320cfe179140410d915706dda1d2db5d91bd05f
MarenceSpeed <44441111mp@gmail.com>
2024-07-14 13:51:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ const texts = [
     "",
     "Da...život je definitivno besmislen...nepravedan i nefer... I to je jasno svakome ko je iole zagrebao ispod površnog i banalnog... Memento mori...pa to da ne možemo zaštiti one koje volimo od patnje...pa tek zla sudbina za dobre ljude...i ostale teške priče koje su jednostavno kalibrisane datosti ovog univerzuma... Tajna je u nečemu drugom... Iako je život sam po sebi besmislen Naš Posao je da mu damo smisao... I tu nema kraja ljudskoj kreativnosti... Od monaha na Svetoj Gori... Preko ljudi zaljubljenih u materijalno... Umetnika i naučnika rešenih da promene svet... Do šankera na Ibici... I svega između... I svako treba da pronađe mesto baš za sebe na toj Lestvici... Baš tamo gde će njemu lično biti dobro...",
     "Potpuno me je oduševila misao koju sam čuo pre neki dan na auto otpadu...od čoveka koji je sa mnom stajao u redu...nalakćen na svog razdrndanog mezimca...i čekao da nas obojicu odere i obrlati vlasnik auto otpada...polučovek...polueuro... Druže...sve je to lepo...avioni...kamioni...skupe limuzine...ali vrednost automobila se jedino meri po tome na koliko si mora i vikend putovanja sa njim otišao...sve ostalo je potpuno nebitno. Kakva duboka životna filozofija... Kakav nasmešeni i pozitivni Mudrac...sa najneočekivanijeg mogućeg mesta...",
-    "Ovo je treći tekst.",
+    "Znaj brzo ce doci kraj vise vremena nema daj veliki zagrljaj i nema problema.",
     "Potreba oba pertnera",
     "Losa energija i razumevanje partnera",
-    "Ovo je četvrti tekst.",
+    "Svet je tvoj i dan i noc cega ces posle da se secas.",
     "Ovo je Peti tekst.",
   ];
   
